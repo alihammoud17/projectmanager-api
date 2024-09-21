5be6dca44ed1166c753c7e7861314c67a278d689
Ali Hammoud <alloochi.hammoud@gmail.com>
2024-09-21 23:43:16 +0300

    projects and tasks api

---
+++ src/controllers/taskController.ts
@@ -0,0 +1,58 @@
+import { Request, Response } from "express";
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export const getTasks = async (req: Request, res: Response) : Promise<void> => {
+    const { projectId } = req.query;
+    try {
+        const projects = await prisma.task.findMany({
+            where: {
+                projectId: Number(projectId)
+            },
+            include: {
+                author: true,
+                assignee: true,
+                comments: true,
+                attachments: true
+            }
+        });
+        res.json(projects);
+    } catch (error: any) {
+        res.status(500).json({ message: "Error retreiving tasks " + error.message });
+    }
+}
+
+export const createTask = async (req: Request, res: Response) : Promise<void> => {
+
+    const { title, description, status, priority, tags, startDate, dueDate, points, projectId, authorUserId, assignedUserId } = req.body;
+
+    try {
+        const newTask = await prisma.task.create({
+            data: {
+                title, description, status, priority, tags, startDate, dueDate, points, projectId, authorUserId, assignedUserId
+            }
+        });
+        res.status(201).json(newTask);
+    } catch (error: any) {
+        res.status(500).json({ message: "Error creating task " + error.message });
+    }
+}
+
+export const updateTaskStatus = async (req: Request, res: Response) : Promise<void> => {
+    const { taskId } = req.params;
+    const { status } = req.body;
+    try {
+        const task = await prisma.task.update({
+            where: {
+                id: Number(taskId)
+            },
+            data: {
+                status
+            }
+        });
+        res.json(task);
+    } catch (error: any) {
+        res.status(500).json({ message: "Error updating tasks " + error.message });
+    }
+}
